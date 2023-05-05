e97ccc213710f9895e25dcd83856aea76f013a6b
Andrea Wang <andrea.wang@sap.com>
2023-05-05 15:15:33 +0800

    move all bundle widgets into non-bundle folder structure

--- Custom-Widget-Samples/GaugeChart/GrogressGauge.js
+++ Custom-Widget-Samples/GaugeChart/GrogressGauge.js
@@ -4,22 +4,22 @@ var getScriptPromisify = (src) => {
   });
 };
 
-const parseMetadata = metadata => {
-  const { dimensions: dimensionsMap, mainStructureMembers: measuresMap } = metadata
-  const dimensions = []
-  for (const key in dimensionsMap) {
-    const dimension = dimensionsMap[key]
-    dimensions.push({ key, ...dimension })
-  }
-  const measures = []
-  for (const key in measuresMap) {
-    const measure = measuresMap[key]
-    measures.push({ key, ...measure })
-  }
-  return { dimensions, measures, dimensionsMap, measuresMap }
-}
-
 (function () {
+  const parseMetadata = metadata => {
+    const { dimensions: dimensionsMap, mainStructureMembers: measuresMap } = metadata
+    const dimensions = []
+    for (const key in dimensionsMap) {
+      const dimension = dimensionsMap[key]
+      dimensions.push({ key, ...dimension })
+    }
+    const measures = []
+    for (const key in measuresMap) {
+      const measure = measuresMap[key]
+      measures.push({ key, ...measure })
+    }
+    return { dimensions, measures, dimensionsMap, measuresMap }
+  }
+
   const template = document.createElement('template')
   template.innerHTML = `
   <style>
