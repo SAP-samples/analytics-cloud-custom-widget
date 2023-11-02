427bf70a0aff790d08a03425f5a9db18debd717b
Marou-Fer <72912738+Marou-Fer@users.noreply.github.com>
2023-11-02 16:19:51 +0100

    Steps Profile

---
+++ Hands-on Steps Profile/Profile Step 3 - Adding Data Binding/main.js
@@ -0,0 +1,42 @@
+
+(function () {
+  const template = document.createElement('template')
+  template.innerHTML = `
+        <style>
+        </style>
+        <div id="root" style="width: 100%; height: 100%;">
+        Hello Custom Widget
+        </div>
+      `
+  class Main extends HTMLElement {
+    constructor () {
+      super()
+
+      this._shadowRoot = this.attachShadow({ mode: 'open' })
+      this._shadowRoot.appendChild(template.content.cloneNode(true))
+
+      this._root = this._shadowRoot.getElementById('root')
+    }
+
+    onCustomWidgetResize (width, height) {
+      this.render()
+    }
+
+    onCustomWidgetAfterUpdate (changedProps) {
+      this.render()
+    }
+
+    onCustomWidgetDestroy () {
+    }
+
+    async render () {
+      const dataBinding = this.dataBinding
+      if (!dataBinding || dataBinding.state !== 'success') {
+        return
+      }
+      this._root.textContent = JSON.stringify(dataBinding)
+    }
+  }
+
+  customElements.define('com-sap-sac-exercise-username-main', Main)
+})()
